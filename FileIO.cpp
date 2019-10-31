8ad029aa9fac2f8668be46f6c3275f31e5e99567
maxwell-wayne-17 <maxwell.wayne.17@cnu.edu>
2019-10-30 21:40:18 -0400

    first push

--- FileIO.cpp
+++ FileIO.cpp
@@ -6,6 +6,8 @@
  */
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
 
@@ -13,12 +15,50 @@ using namespace std;
 
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	contents.clear();
+
+	ifstream file;
+	file.open(filename);
+
+	if (!file.is_open()){
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+
+	string line;
+	while (getline(file, line)) {
+
+		stringstream ss(line);
+
+		getline(ss, line);
+		contents += line;
+
+		ss.clear();
+	}
+
+	file.close();
+
+	return SUCCESS;
 }
 
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	fstream file;
+	file.open(filename, ios_base::out);
+
+	if (!file.is_open()){
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+	}
+
+	std::vector<std::string>::iterator vectorItr;
+	for(vectorItr = myEntryVector.begin();
+			vectorItr != myEntryVector.end();
+			vectorItr++){
+		file << *vectorItr<< endl;
+	}
+
+	file.close();
+
+	return SUCCESS;
 }
 
 
